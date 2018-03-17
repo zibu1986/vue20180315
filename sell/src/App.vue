2752982b6d9eef6b6c05cefd78c573d7d255a3e0
zibu <849616922@qq.com>
2018-03-18 07:11:11 +0800

    Initial commit

---
+++ sell/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+ <div>
+   <vheader></vheader>
+   <div class="tab">
+   <router-link to="/Foo">商品</router-link>
+   <router-link to='/Bar'>评论</router-link>
+   <router-link to="/sel">商家</router-link>
+  </div>
+  <router-view></router-view>
+   <div class="content">i am content</div>
+ </div>
+ 
+</template>
+
+<script>
+import header from './components/header/head.vue'
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+const sel = { template: '<div>sel</div>' }
+export default{
+  components:{
+    "vheader":header,
+    Foo,
+    Bar,
+    sel
+  }
+}
+</script>
+
+<style>
+.tab{
+  display: flex;
+  width:100%;
+  height: 40px;
+  line-height: 40px;
+  justify-content: space-around;
+}
+.tab-item{
+   flex: 1;
+  text-align: center;
+}
+</style>
---
+++ sell/src/components/header/head.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+      <div>
+我是header；
+      </div>
+
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+
+}
+</script>
+<style   type="text/css">
+ .header{
+   
+ }
+</style>
+
+
+
+
+
+
+
+
